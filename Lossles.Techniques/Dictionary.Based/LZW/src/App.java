ed187d8067fba483c4dcff4ae91820e116eaa617
Yassin-Ramadan <y.ramadan1118@gmail.com>
2024-11-04 20:34:28 +0200

    Added the LZW compression Function and added a file to take input from

--- Lossles.Techniques/Dictionary.Based/LZW/src/App.java
+++ Lossles.Techniques/Dictionary.Based/LZW/src/App.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        
+
+        Scanner scanner = new Scanner(System.in);
+        String filePath = scanner.nextLine();
+        scanner.close();
+        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        String stream = new String();
+        for (String line : lines)
+            stream = stream + line;
+
+        ArrayList<Integer> tags = LZW.Compress(stream);
+
+        for(int i = 0; i < tags.size(); i++)
+            System.out.print("< " + tags.get(i) + " >   ");
         
     }
 }
--- Lossles.Techniques/Dictionary.Based/LZW/src/LZW.java
+++ Lossles.Techniques/Dictionary.Based/LZW/src/LZW.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
 public class LZW {
 
+
+    public static ArrayList<Integer> Compress(String stream) {
+        
+        ArrayList<Integer> tags = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        String lookFor;
+        int nextIndex = 128;
+        int tag = -1;
+        int matchSize = 1;
+        for(int left = 0; left < stream.length(); left += matchSize){ 
+            tag = -1;
+            matchSize = 1;
+            for(int lookForSize = 1; lookForSize < stream.length() - left + 1; lookForSize++) {
+                if(lookForSize == 1){
+                    tag = (int) stream.charAt(left);
+                    continue;
+                }
+                lookFor = stream.substring(left, left + lookForSize);
+                if(map.containsKey(lookFor)) {
+                    tag = map.get(lookFor);
+                    matchSize = lookForSize;
+                }
+                else {
+                    map.put(lookFor, nextIndex++);
+                    break;
+                }
+            }
+            tags.add(tag);
+        }
+        return tags;
+    }
 }
